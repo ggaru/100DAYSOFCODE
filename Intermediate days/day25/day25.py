a850ee9a90357c6ab401344c6c5973ea4e27caf4
garu <ggaru.dev@gmail.com>
2026-06-02 20:26:16 -0300

    #feat_v - finalizado dia 25

--- Intermediate days/day25/day25.py
+++ Intermediate days/day25/day25.py
@@ -1,21 +1,60 @@
-
-# with open("C:\\Users\\vinic\\OneDrive\\CODE\\PY\\A100DAYSOFCODE\\Intermediate days\\day25\\weather_data.csv", "r") as file:
-#     data = file.readlines()
-#     list = []
-#     for i in data:    
-#         list.append(i.strip())
-#     print(list)
-    
-# import csv
-
-# with open("Intermediate days\\day25\\weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp": temperatures.append(row[1])
-#     print(temperatures)
-
+from turtle import Turtle, Screen
 import pandas as pd
 
-data = pd.read_csv("Intermediate days\\day25\\weather_data.csv")
-print(data)
+
+screen = Screen()
+turtle = Turtle()
+screen.title("U.S. States Game")
+playing = True
+states_corrects = []
+
+
+
+doc = pd.read_csv("Intermediate days/day25/50_states.csv")
+states_to_learn = []
+for i in doc["state"]:
+    states_to_learn.append(i)
+img = "Intermediate days\\day25\\blank_states_img.gif"
+screen.addshape(img)
+turtle.shape(img)
+
+
+def find_state(answer):
+    
+    if answer_state not in states_corrects:
+        for i in doc['state']:
+            if i == answer_state:
+                states_corrects.append(i)
+                states_to_learn.remove(i)
+                for x in doc[doc['state'] ==i].x: getx = x
+                for y in doc[doc['state'] ==i].y: gety = y
+                show_state(answer_state, getx, gety)
+    
+            
+            
+def show_state(name, x,y):
+    state = Turtle()
+    state.hideturtle()
+    state.penup()
+    state.goto(x,y)
+    state.write(name)
+    
+
+while playing:
+    if len(states_corrects) != 50:
+        score = len(states_corrects)
+        answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another states name?")
+        answer_state = answer_state.title()
+        if answer_state == "Exit":
+            file = open("Intermediate days/day25/states to learn.csv", "w")
+            for i in states_to_learn: 
+                file.write(f"{i}\n")
+            file.close()
+            playing = False
+            break
+        else:
+            find_state(answer_state)
+    else:
+        print("Congratulations! You won!")
+
+screen.mainloop()
